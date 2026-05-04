51c0ab199226c970cc5a37abc967d8f1ce4c934f
Josh Welborn <jswelborn88@hotmail.com>
2026-05-04 13:44:32 -0400

    chore: testing real payload shape from HCP

--- main.tf
+++ main.tf
@@ -1,9 +1,8 @@
-/*
 locals {
   resource_group_name = "rg-80020000-ansible"
   location            = "East US"
   subnet_id           = "/subscriptions/6133c7a0-a417-43b9-acb9-1bdd65114bbe/resourceGroups/ht-hts-eastus-core-rg/providers/Microsoft.Network/virtualNetworks/ht-hts-eastus-vnet1/subnets/ht-hts-eastus-utilityservices-app-prod-subnet-private"
-  vm_name             = "hts001cldans009"
+  vm_name             = "hts001cldans010"
 }
 
 resource "azurerm_network_interface" "nic" {
@@ -49,4 +48,3 @@ resource "azurerm_linux_virtual_machine" "vm" {
     version   = "latest"
   }
 }
-*/
