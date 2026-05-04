ef256eacbdc975706af00341ef88886abc4a5359
Josh Welborn <jswelborn88@hotmail.com>
2026-05-04 15:17:44 -0400

    chore: add hts001cldans010 for testing

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ locals {
   resource_group_name = "rg-80020000-ansible"
   location            = "East US"
   subnet_id           = "/subscriptions/6133c7a0-a417-43b9-acb9-1bdd65114bbe/resourceGroups/ht-hts-eastus-core-rg/providers/Microsoft.Network/virtualNetworks/ht-hts-eastus-vnet1/subnets/ht-hts-eastus-utilityservices-app-prod-subnet-private"
-  vm_name             = "hts001cldans011"
+  vm_name             = "hts001cldans010"
 }
 
 resource "azurerm_network_interface" "nic" {
