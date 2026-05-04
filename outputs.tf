86bc5e2db6338c745f9dc34b6574cb0fb0b0f040
Josh Welborn <jswelborn88@hotmail.com>
2026-05-04 13:37:39 -0400

    chore: remove hts001cldans009 test

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,4 @@
+/*
 output "vm_name_terraformvms" {
   value = [azurerm_linux_virtual_machine.vm.name]
 }
@@ -5,3 +6,4 @@ output "vm_name_terraformvms" {
 output "vm_private_ip" {
   value = azurerm_network_interface.nic.private_ip_address
 }
+*/
