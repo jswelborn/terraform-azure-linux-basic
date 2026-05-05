e1c5e1507cb6d5280dc6a8734709767b4ab54f84
Josh Welborn <jswelborn88@hotmail.com>
2026-05-05 14:38:22 -0400

    chore: deploying hts001cldans012 for test

--- locals.tf
+++ locals.tf
@@ -4,5 +4,5 @@ locals {
   subnet_id           = "/subscriptions/6133c7a0-a417-43b9-acb9-1bdd65114bbe/resourceGroups/ht-hts-eastus-core-rg/providers/Microsoft.Network/virtualNetworks/ht-hts-eastus-vnet1/subnets/ht-hts-eastus-utilityservices-app-prod-subnet-private"
 
   linux_vm_name   = "hts001cldans010"
-  windows_vm_name = "hts001cldans011"
+  windows_vm_name = "hts001cldans012"
 }
