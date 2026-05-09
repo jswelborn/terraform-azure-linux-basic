493e2bbb60fac3fa0d6c7106ec83fbb5204d0d8d
Josh Welborn <jswelborn88@hotmail.com>
2026-05-09 09:30:23 -0400

    chore: add hts001cldans015

--- hts001cldans015.tf
+++ hts001cldans015.tf
@@ -1,11 +1,34 @@
-/*
 resource "aws_instance" "windows_instance" {
   ami                    = "ami-0798c9ebaf695e14e"
   instance_type          = "t3.large"
   subnet_id              = "subnet-9751e7ce"
   vpc_security_group_ids = ["sg-4d963729"]
   key_name               = "HETLX_ETG_US_EAST_1"
-  get_password_data      = true
+
+  user_data = <<-EOF
+<powershell>
+$ErrorActionPreference = "Stop"
+
+$Username = "hetwinadmin"
+$Password = ConvertTo-SecureString "${var.hetwinadmin_default_password}" -AsPlainText -Force
+
+if (-not (Get-LocalUser -Name $Username -ErrorAction SilentlyContinue)) {
+  New-LocalUser -Name $Username -Password $Password -PasswordNeverExpires -AccountNeverExpires
+}
+
+Add-LocalGroupMember -Group "Administrators" -Member $Username -ErrorAction SilentlyContinue
+
+winrm quickconfig -q
+winrm set winrm/config/service/auth '@{Basic="true"}'
+winrm set winrm/config/service '@{AllowUnencrypted="true"}'
+
+Enable-PSRemoting -Force
+Set-Service WinRM -StartupType Automatic
+Start-Service WinRM
+
+New-NetFirewallRule -DisplayName "Allow WinRM HTTP" -Direction Inbound -Protocol TCP -LocalPort 5985 -Action Allow -ErrorAction SilentlyContinue
+</powershell>
+EOF
 
   root_block_device {
     volume_size = 128
@@ -14,5 +37,3 @@ resource "aws_instance" "windows_instance" {
 
   tags = local.aws_windows_tags
 }
-
-*/
