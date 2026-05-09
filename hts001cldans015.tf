1eab0f4ebf87699fc9514040433481a45118cda0
Josh Welborn <jswelborn88@hotmail.com>
2026-05-08 23:42:13 -0400

    chore: adding hts001cldans015

--- hts001cldans015.tf
+++ hts001cldans015.tf
@@ -1,4 +1,3 @@
-/*
 resource "aws_instance" "windows_instance" {
   ami                    = "ami-0798c9ebaf695e14e"
   instance_type          = "t3.large"
@@ -13,4 +12,3 @@ resource "aws_instance" "windows_instance" {
 
   tags = local.aws_windows_tags
 }
-*/
