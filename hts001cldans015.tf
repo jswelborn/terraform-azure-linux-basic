fe265d0cd2c6acecc151770dd02dcf8e2cba9b87
Josh Welborn <jswelborn88@hotmail.com>
2026-05-09 09:23:40 -0400

    chore: remove hts001cldans015

--- hts001cldans015.tf
+++ hts001cldans015.tf
@@ -1,9 +1,11 @@
+/*
 resource "aws_instance" "windows_instance" {
   ami                    = "ami-0798c9ebaf695e14e"
   instance_type          = "t3.large"
   subnet_id              = "subnet-9751e7ce"
   vpc_security_group_ids = ["sg-4d963729"]
   key_name               = "HETLX_ETG_US_EAST_1"
+  get_password_data      = true
 
   root_block_device {
     volume_size = 128
@@ -12,3 +14,5 @@ resource "aws_instance" "windows_instance" {
 
   tags = local.aws_windows_tags
 }
+
+*/
